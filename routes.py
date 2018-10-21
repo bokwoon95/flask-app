d6f948119f419b226d74885a11aeca5038cae2c1
qwetuo <chewminzhuang@hotmail.com>
2018-10-21 14:01:15 +0800

    added routes for signupform and showing of data

---
+++ routes.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+users = [{"firstname":"a","lastname":"b","email":"@mail.com", "countries":"countries", "contactnumber":"123","password":"password"}]
+
+@app.route("/form")
+def signupform():
+    return render_template('start.html')
+
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    user = {"firstname": request.form['firstname'],"lastname": request.form['lastname'],"email": request.form["email1"], "countries": request.form['countries'],"contactnumber": request.form['contactnumber'],"password": request.form["password1"]}
+    global users
+    users.append(user)
+    return redirect('/data')
+
+@app.route('/data')
+def data():
+      return render_template(
+            'userdata.html', users=users)
