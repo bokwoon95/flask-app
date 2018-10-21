5a6b1c0b1e528f7b30c2b602fdbfc9c65ab6c715
bokwoon <bokwoon.c@gmail.com>
2018-10-21 21:04:20 +0800

    Further attempts at deployment

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from app import app
 import pdb
+import os
 
 # app = Flask(__name__)
 
@@ -102,3 +103,7 @@ def result():
 def data():
       return render_template(
             'userdata.html', users=users)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
